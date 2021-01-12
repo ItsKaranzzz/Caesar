c2d3abfe6434aa697cd95e8afcc73236c1735258
Karan Chaudhary <karan.c.aux@go-jek.com>
2021-01-12 18:34:30 +0530

    Karan | Gofood | Added home page locators to the existing tests

--- test/web/signInTest.js
+++ test/web/signInTest.js
@@ -1,22 +1,27 @@
 const { assert } = require('chai')
+const homePage = require('../../src/pages/HomePage')
 const signInPage = require('../../src/pages/signInPage')
 const constants = require('../../src/constants/PageTitles')
 
-describe('Sign in page feature', function(){
-
-    it('verify sign in page title'), function(){
-        browser.url('/')
-        browser.maximizeWindow()
-        const actualTitle = signInPage.getPageTitle()
+describe("Sign in page feature", function(){
+    
+    it('verify sign in page title', function(){
+        browser.url('/');
+        browser.maximizeWindow();
+        homePage.clickOnSignInBtn();
+        const actualTitle = signInPage.getPageTitle(constants.SIGN_IN_PAGE_TITLE);
         console.log(actualTitle)
-        assert.equal(actualTitle, constants.SIGN_IN_PAGE_TITLE,'Sign page title was not expected!')
-    }
+        assert.equal(actualTitle, constants.SIGN_IN_PAGE_TITLE,'Sign page title was not expected!');  
+    });
 
-    it('verify sign in to the app'),function(){
+    it('verify sign in to the app', function(){
         browser.url('/')
         browser.maximizeWindow()
+        homePage.clickOnSignInBtn()
         signInPage.signIn('asda','asdas')
         assert.isTrue(signInPage.isInvalidLogin,'Invalid login message did not appear')
         assert.equal(signInPage.getInvalidLoginMessage, constants.INVALID_SIGN_IN_MESSAGE, 'invalid sign in message was incorrect!')
-    }
-})
+      });
+
+    
+});
