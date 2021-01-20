83f3a58d3a3aa29a053d4dbc4151ad1ded595df4
caesar js <caesar.testjs@gmail.com>
2021-01-20 07:43:10 +0000

    Kavitha | Adding test for creating project and Tags and gitIgnore file

--- test/api/accessTokenTest.js
+++ test/api/accessTokenTest.js
@@ -2,29 +2,36 @@ const chai = require("chai");
 const expect = chai.expect;
 const request = require("../../src/utils/requestBody");
 const header = require("../../src/utils/header");
-const config = require("../../src/config");
+const getUrl = require("../../src/configs/url");
 
 var log4js = require("log4js");
-var log = require("../../src/utils/logger")
+var log = require("../../src/utils/logger");
 log4js.configure(log.logging());
 var logger = log4js.getLogger();
 
 describe("Get user details", () => {
-  let baseUrl = config.apiBaseUrl;
+  let baseUrl = getUrl.apiBaseUrl;
   let url;
   let headers = header.plainHeader();
 
-  it("Verify user details using private access token", () => {
+  it("Verify user details using private access token @smoke", (done) => {
     let uri = "/personal_access_tokens";
     url = baseUrl + uri;
-    logger.info("The access token url is", url)
+    logger.info("The access token url is", url);
     request
-    . requestPromiseQuery(url,'GET',headers)
+      .requestPromiseQuery(url, "GET", headers)
       .then((response) => {
-        logger.info("Number of Records in GET Service API with url", url, "is", response.body.length)
+        logger.info(
+          "status code in GET Service API with url ",
+          url,
+          "is ",
+          JSON.stringify(response.statusCode)
+        );
         expect(response.statusCode).to.equal(200);
-        expect(response.body.length).to.be.at.least(100)
       })
-    })
+      .then(() => done(), done)
+      .catch((error) => {
+        done(error);
+      });
+  });
 });
-
