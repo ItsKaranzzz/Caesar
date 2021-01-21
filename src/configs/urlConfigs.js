3ff4b8d3afb38b6edcf9cc9bbd007ec79c67e7a2
Karan Chaudhary <karan.c.aux@go-jek.com>
2021-01-21 12:08:51 +0530

    Karan | Updated the uri to support project name and members dynamically

--- src/configs/urlConfigs.js
+++ src/configs/urlConfigs.js
@@ -3,8 +3,6 @@ module.exports =
     apiBaseUrl : "https://gitlab.com/api/v4",
     projectApiUri:"/projects",
     personalAccessTokenApiUri:"/personal_access_tokens",
-    addNewMemberToProjectUri: "/projects/23698532/members",
-    //TODO Need to handle passing project Id as Path variable
-    deleteMember: "/projects/23698532/members/6884876"
-
+    addNewMemberToProjectUri: "/projects/%s/members",
+    deleteMember: "/projects/%s/members/%s"
 }
